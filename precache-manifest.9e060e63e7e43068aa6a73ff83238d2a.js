c1f6a4a83ac74a7e5473300bde6bcb4493873046
unknown <azimov3.9.1991@gmail.com>
2020-10-17 15:17:21 +0300

    Updates

---
+++ precache-manifest.9e060e63e7e43068aa6a73ff83238d2a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b91d5ad720d4d708401ae464110622d2",
+    "url": "/goit-react-hw-03-phonebook/index.html"
+  },
+  {
+    "revision": "db2eacd64e336fbb477b",
+    "url": "/goit-react-hw-03-phonebook/static/css/main.345c65f4.chunk.css"
+  },
+  {
+    "revision": "f3bbbdae99c3d3740f23",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.77afbb66.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.77afbb66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db2eacd64e336fbb477b",
+    "url": "/goit-react-hw-03-phonebook/static/js/main.c7eec356.chunk.js"
+  },
+  {
+    "revision": "159f9e7c304b75423aa5",
+    "url": "/goit-react-hw-03-phonebook/static/js/runtime-main.cca09c81.js"
+  }
+]);
